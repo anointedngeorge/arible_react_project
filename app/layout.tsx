0fd625d735ed2a0bd65542791d073d02cd41c5a5
anointedngeorge <anointedngeorge@gmail.com>
2025-01-11 11:56:44 +0100

    updated the component/header.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'The Arible Estate & Properties',
+  description: 'The Arible Estate & Properties',
 }
 
 export default function RootLayout({
